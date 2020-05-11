29f8168eec8758816228ca7f94d5210cf74e9010
Ricky Mao <r23mao@uwaterloo.ca>
2020-05-10 20:55:21 -0700

    styling test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Login from './components/Login';
 import dashboard from './components/Dashboard';
 import Dashboard from './components/Dashboard';
+import styled from 'styled-components';
+
+const Background = styled.div`
+
+background: red;
+width: 100px;
+height: 100px;
+
+
+`;
 
 function App() {
 
   return (
 
     <BrowserRouter>
+
+    <Background></Background>
 
     <Route exact path='/' component={Login}/>
     <Route exact path='/dashboard' component={Dashboard}/>
